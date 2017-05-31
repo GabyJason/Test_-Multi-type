328c1e90aeaceb7a6af6c13ea36253e04405830b
user <user@DESKTOP-RAEKJ4K>
2017-06-01 00:18:49 +0800

    Test_多型

--- Test_Multi-type/Program.cs
+++ Test_Multi-type/Program.cs
@@ -8,8 +8,44 @@ namespace Test_Multi_type
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Parent p = new Parent();
+            p.Name = "parent";
+            TestPolymorphism(p);
+
+            Child c = new Child();
+            c.Name = "child";
+            TestPolymorphism(c);
+        }
+
+        static void TestPolymorphism(Parent parent)
+        {
+            parent.Print();
+        }
+
+        /// <summary>
+        /// virtual 可使 override 覆蓋
+        /// </summary>
+        public class Parent
+        {
+            public string Name { get; set; }
+
+            public virtual void Print()
+            {
+                Console.WriteLine("Parent Name: " + Name);
+            }
+        }
+
+        /// <summary>
+        /// 覆蓋繼承
+        /// </summary>
+        public class Child : Parent
+        {
+            override public void Print()
+            {
+                Console.WriteLine("Child Name: " + Name);
+            }
         }
     }
 }
